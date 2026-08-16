8893519e6a76c6bdec3d9dfa590d52da9509f5ad
Mathias Giller <mathias.giller@icloud.com>
2026-08-16 20:59:17 +0200

    docs: add TODO comments for GameManager

--- src/main/java/org/nooberextrim/skyblockwars/GameManager.java
+++ src/main/java/org/nooberextrim/skyblockwars/GameManager.java
@@ -4,6 +4,7 @@ public class GameManager {
 
     private final Game game;
     private final TeamManager teamManager;
+    //TODO :: use GameManager or insure that the same TeamManager gets passed to SkyblockCommand
     private final TeleportManager teleportManager;
 
     public GameManager(Game game,  TeamManager teamManager, TeleportManager teleportManager) {
@@ -11,4 +12,6 @@ public class GameManager {
         this.teamManager = teamManager;
         this.teleportManager = teleportManager;
     }
+
+    //TODO :: add game flow methods for starting game, teleporting to islands ending game and resetting map.
 }
